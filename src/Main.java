107495244c8dbdd824cd7c8d629f3a1d8899914c
Egor <egor.volkov20038@gmail.com>
2023-02-28 17:09:35 +0300

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.concurrent.Future;
+
 public class Main {
     public static void main(String[] args) {
-        // template
+        MyExecutorService myExecutorService = new MyExecutorService(2);
+        myExecutorService.execute(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Потом это");
+        });
+
+        myExecutorService.execute(() -> System.out.println("Сначала это"));
+        myExecutorService.shutdown();
     }
 }

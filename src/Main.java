d887ea71e70c9cd0b47ea4882520d7cdafb32466
Egor <egor.volkov20038@gmail.com>
2023-03-01 15:35:22 +0300

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        // template
+        Singleton1 singleton1_1 = Singleton1.getInstance();
+        Singleton1 singleton1_2 = Singleton1.getInstance();
+
+        Singleton2 singleton2_1 = Singleton2.getInstance();
+        Singleton2 singleton2_2 = Singleton2.getInstance();
+        Singleton2 singleton2_3 = Singleton2.INSTANCE;
+
+        Singleton3 singleton3_1 = Singleton3.getInstance();
+        Singleton3 singleton3_2 = Singleton3.getInstance();
+
+        System.out.println("Первый вариант:");
+        System.out.println(singleton1_1.hashCode() + " " + singleton1_2.hashCode());
+
+        System.out.println("Второй вариант:");
+        System.out.println(singleton2_1.hashCode() + " " + singleton2_2.hashCode() + " "
+                + singleton2_3.hashCode());
+
+        System.out.println("Третий вариант:");
+        System.out.println(singleton3_1.hashCode() + " " + singleton3_2.hashCode());
+
     }
 }
---
+++ src/Singleton2.java
@@ -0,0 +1,6 @@
+public enum Singleton2 {
+    INSTANCE;
+    public static Singleton2 getInstance() {
+        return INSTANCE;
+    }
+}
